15367ae873f39faad82a9652bdff646dc4d82576
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-16 16:21:01 +0100

    add seeder with types array

--- database/migrations/2023_01_16_150230_add_foreing_type_id_to_projects_table.php
+++ database/migrations/2023_01_16_150230_add_foreing_type_id_to_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->insignedBigInteger('type_id')->references('id')->nullable()->after('id');
+            $table->unsignedBigInteger('type_id')->references('id')->nullable()->after('id');
             $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
         });
     }
